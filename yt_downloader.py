5261def48742ab34c5b49609b5a68d143167cfe2
unknown <shubhamphadte37@gmail.com>
2019-02-25 11:24:35 +0530

    youtube downloader- the first of many

---
+++ yt_downloader.py
@@ -0,0 +1,15 @@
+from pytube import YouTube 
+x=input("Enter the URL of the video to be downloaded: ")
+print("")
+print("Downloading..........")
+x="'"+x+"'"
+yt=YouTube(x)
+print("")
+print(yt.title)
+some_video=yt.streams.first()
+
+
+some_video.download()
+print("")
+print("Download Successful !!")
+
