e36a9da3eeb66f1cffa377429bf0118ac7a905bd
shubham-rp <shubhamphadte37@gmail.com>
2020-09-27 11:39:08 +0530

    Refining the Code

--- yt_downloader.py
+++ yt_downloader.py
@@ -2,10 +2,12 @@ from pytube import YouTube
 x=input("Enter the URL of the video to be downloaded: ")
 print("")
 print("Downloading..........")
-x="'"+x+"'"
+
 yt=YouTube(x)
 print("")
+
 print(yt.title)
 yt.streams.first().download()
+
 print("")
 print("Download Successful !!")
